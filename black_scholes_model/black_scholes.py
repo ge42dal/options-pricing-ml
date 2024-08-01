fe64972b2ce9e3e142b0b3b3514157cdc2987fcd
ge42dal <luis.gonzales26902@gmail.com>
2024-08-01 16:31:32 +0200

    first MSE that doesnt explode

--- black_scholes_model/black_scholes.py
+++ black_scholes_model/black_scholes.py
@@ -2,12 +2,7 @@ import pandas as pd
 import numpy as np
 from scipy import stats
 
-try:
-    from IPython import get_ipython
-    get_ipython().magic('clear')
-    get_ipython().magic('reset -f')
-except:
-    pass
+from utils.utlis import calculate_metrics
 
 def d1(S, X, r, sigma, T):
     return (np.log(S / X) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
@@ -38,39 +33,8 @@ def put_price(row):
     two = d2(one, sigma, ttm)
     return X * np.exp(-r * ttm) * stats.norm.cdf(-two) - S * stats.norm.cdf(-one)
 
-def calculate_mse(true, predicted):
-    return np.mean((predicted - true) ** 2)
-
-def calculate_rmse(true, predicted):
-    return np.sqrt(calculate_mse(true, predicted))
-
-def calculate_mae(true, predicted):
-    return np.mean(np.abs(predicted - true))
-
-def calculate_mape(true, predicted):
-    return np.mean(np.abs(predicted - true) / predicted)
-
-def calculate_r2_score(true, predicted):
-    ss_res = np.sum((predicted - true) ** 2)
-    ss_tot = np.sum((true - true.mean()) ** 2)
-    return 1 - (ss_res / ss_tot)
 
 
-def calculate_call_metrics(calls:pd.DataFrame):
-    mse = calculate_mse(calls['bid_eod'], calls['Predicted_Call_price'])
-    rmse = calculate_rmse(calls['bid_eod'], calls['Predicted_Call_price'])
-    mae = calculate_mae(calls['bid_eod'], calls['Predicted_Call_price'])
-    mape = calculate_mape(calls['bid_eod'], calls['Predicted_Call_price'])
-    r2 = calculate_r2_score(calls['bid_eod'], calls['Predicted_Call_price'])
-    return mse, rmse, mae, mape, r2
-
-def calculate_put_metrics(puts:pd.DataFrame):
-    mse = calculate_mse(puts['ask_eod'], puts['Predicted_Put_Price'])
-    rmse = calculate_rmse(puts['ask_eod'], puts['Predicted_Put_Price'])
-    mae = calculate_mae(puts['ask_eod'], puts['Predicted_Put_Price'])
-    mape = calculate_mape(puts['ask_eod'], puts['Predicted_Put_Price'])
-    r2 = calculate_r2_score(puts['ask_eod'], puts['Predicted_Put_Price'])
-    return mse, rmse, mae, mape, r2
 
 if __name__ == "__main__":
     options_data = pd.read_csv('../clean_data/options_free_dataset.csv')
@@ -81,8 +45,8 @@ if __name__ == "__main__":
     calls['Predicted_Call_price'] = calls.apply(call_price, axis=1)
     puts['Predicted_Put_Price'] = puts.apply(put_price, axis=1)
 
-    cmse, crmse, cmae, cmape, cr2 = calculate_call_metrics(calls)
-    pmse, prmse, pmae, pmape, pr2 = calculate_put_metrics(puts)
+    cmse, crmse, cmae, cmape, cr2 = calculate_metrics(calls['bid_eod'], calls['Predicted_Call_price'])
+    pmse, prmse, pmae, pmape, pr2 = calculate_metrics(puts['ask_eod'], puts['Predicted_Put_Price'])
 
     metrics = {
         'Metric': ['MSE', 'RMSE', 'MAE', 'MAPE', 'R2'],
--- lstm/LSTM.py
+++ lstm/LSTM.py
@@ -1,27 +1,38 @@
 import pathlib
+from pathlib import Path
 
+import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.optim import Adam
+from torch.utils.data import DataLoader, TensorDataset
 
 from sklearn.model_selection import train_test_split
-
-from pathlib import Path
+from tqdm import tqdm
 
 import pandas as pd
 import numpy as np
 
+from utils.utlis import calculate_metrics
 
-def preprocess_data(path_options: pathlib.Path, path_underlying: pathlib.Path) -> (
+
+def preprocess_data(path_options: pathlib.Path, path_underlying: pathlib.Path, option_type: str) -> (
         pd.DataFrame, pd.DataFrame, pd.DataFrame):
     days_to_roll = 20
 
     options_data = pd.read_csv(path_options)
     underlying_data = pd.read_csv(path_underlying)
+    if option_type == 'c':
+        options_data.drop(['Sigma_20_Days_Annualized', "Underlying_Price", "ask_eod", "Option_Average_Price"], axis=1, inplace=True)
+        options_data = options_data[options_data['OptionType'] =='c']
 
-    options_data.drop(['Sigma_20_Days_Annualized', "Underlying_Price", "bid_eod", "ask_eod"], axis=1, inplace=True)
-
+    elif option_type == 'p':
+        options_data.drop(['Sigma_20_Days_Annualized', "Underlying_Price", "bid_eod", "Option_Average_Price"], axis=1,
+                          inplace=True)
+        options_data = options_data[options_data['OptionType'] == 'p']
+    else:
+        raise ValueError('Option type must be c or p')
     # creation of 20 day time series (n,m) matrix to feed in to LSTM layer, where n are individual sample prices of
     # the underlying at different dates, and m represent the previous 20 days from most recent to furthest back
     padded = np.insert(underlying_data[" Close"].values, 0,
@@ -37,42 +48,47 @@ def preprocess_data(path_options: pathlib.Path, path_underlying: pathlib.Path) -
     joined_df = options_data.join(stacked)
     joined_df.dropna(axis=0, inplace=True)
     final_df = joined_df.apply(pd.to_numeric, errors='ignore')
+    final_df.drop(columns=['OptionType'], inplace=True)
+    return final_df
 
-    calls_df = final_df[final_df['OptionType'] == 'c'].drop(columns='OptionType')
-    put_df = final_df[final_df['OptionType'] == 'p'].drop(columns='OptionType')
+def get_train_test_split(data: pd.DataFrame, option_type: str) -> (np.array, np.array, np.array, np.array):
+    if option_type == 'c':
+        X = data.drop(columns=['bid_eod'])
+        y = data['bid_eod']
+    elif option_type == 'p':
+        X = data.drop(columns=['ask_eod'])
+        y = data['ask_eod']
+    else:
+        raise ValueError('Option type must be c or p')
 
-    return final_df, calls_df, put_df
-
-
-def get_train_test_split(data: pd.DataFrame) -> (np.array, np.array, np.array, np.array):
-    X = data.drop(columns=['Option_Average_Price'])
-    y = data['Option_Average_Price']
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, shuffle=True)
 
     return X_train, X_test, y_train, y_test
 
 
 class LSTMTrain(nn.Module):
 
-    def __init__(self, input_size=1, hidden_size=50, num_layers=4, output_size=1, n_features=4, n_neurons=50):
+    def __init__(self, lstm_input_size=1, lstm_hidden_size=50, lstm_n_layers=4, output_size=1, fc_n_features=4, fc_n_neurons=400):
         super().__init__()
-        self.num_layers = num_layers
-        self.hidden_size = hidden_size
-        self.input_size = input_size
+        self.num_layers = lstm_n_layers
+        self.hidden_size = lstm_hidden_size
+        self.input_size = lstm_input_size
         self.output_size = output_size
-        self.n_features = n_features
-        self.n_neurons = n_neurons
+        self.n_features = fc_n_features
+        self.n_neurons = fc_n_neurons
 
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc_lstm = nn.Linear(hidden_size, output_size)
+        self.lstm = nn.LSTM(lstm_input_size, lstm_hidden_size, lstm_n_layers, batch_first=True)
 
-        self.fc1 = nn.Linear(n_features, n_neurons)
-        self.fc2 = nn.Linear(n_neurons, n_neurons)
-        self.fc3 = nn.Linear(n_neurons, n_neurons)
-        self.fc4 = nn.Linear(n_neurons, output_size)
+        self.fc_lstm = nn.Linear(lstm_hidden_size, output_size)
 
-        self.ReLU = nn.ReLU()
+        self.fc1 = nn.Linear(fc_n_features, fc_n_neurons)
+        self.bn1 = nn.BatchNorm1d(fc_n_neurons)
+        self.fc2 = nn.Linear(fc_n_neurons, fc_n_neurons)
+        self.bn2 = nn.BatchNorm1d(fc_n_neurons)
+        self.fc3 = nn.Linear(fc_n_neurons, fc_n_neurons)
+        self.bn3 = nn.BatchNorm1d(fc_n_neurons)
+        self.fc4 = nn.Linear(fc_n_neurons, output_size)
+        self.leaky_relu = nn.LeakyReLU()
 
     def forward(self, x_lstm, x_dense):
         lstm_out = self.lstm_part(x_lstm)
@@ -81,11 +97,18 @@ class LSTMTrain(nn.Module):
         return dense_out
 
     def dense_part(self, x):
-        fc1 = self.fc1(x)
-        fc2 = self.fc2(fc1)
-        fc3 = self.fc3(fc2)
-        fc4 = self.fc4(fc3)
-        return fc4
+        x = self.leaky_relu(x)
+        x = self.fc1(x)
+        x = self.bn1(x)
+        x = self.leaky_relu(x)
+        x = self.fc2(x)
+        x = self.bn2(x)
+        x = self.leaky_relu(x)
+        x = self.fc3(x)
+        x = self.bn3(x)
+        x = self.leaky_relu(x)
+        x = self.fc4(x)
+        return x
 
     def lstm_part(self, x):
         batch_size = x.size(0)  # 26205 total dates of prices
@@ -99,26 +122,78 @@ class LSTMTrain(nn.Module):
 
 
 def training_loop(epochs: int, model: nn.Module, X_train: pd.DataFrame,
-                  y_train: pd.DataFrame):
-    lstm_part_tensor = torch.tensor(X_train.drop(columns=['strike', 'Time_to_Maturity', 'RF_Rate']).values,
+                  y_train: pd.DataFrame, X_test: pd.DataFrame, y_test: pd.DataFrame,):
+
+    lstm_train_tensor = torch.tensor(X_train.drop(columns=['strike', 'Time_to_Maturity', 'RF_Rate']).values,
                                     dtype=torch.float32)
-    dense_part = torch.tensor(X_train[['strike', 'Time_to_Maturity', 'RF_Rate']].values, dtype=torch.float32)
-    true = torch.tensor(y_train.values, dtype=torch.float32)
-    loss_vec = np.zeros(epochs)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    dense_train_tensor = torch.tensor(X_train[['strike', 'Time_to_Maturity', 'RF_Rate']].values, dtype=torch.float32)
+    train_targets = torch.tensor(y_train.values, dtype=torch.float32)
+
+    lstm_test_tensor = torch.tensor(X_test.drop(columns=['strike', 'Time_to_Maturity', 'RF_Rate']).values, dtype=torch.float32)
+    dense_test_tensor = torch.tensor(X_test[['strike', 'Time_to_Maturity', 'RF_Rate']].values, dtype=torch.float32)
+    test_targets = torch.tensor(y_test.values, dtype=torch.float32)
+
+    train_dataset = TensorDataset(lstm_train_tensor, dense_train_tensor,  train_targets)
+    test_dataset = TensorDataset(lstm_test_tensor, dense_test_tensor, test_targets)
+    train_loader = DataLoader(train_dataset, batch_size=512, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=512, shuffle=False)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
     loss_fn = torch.nn.MSELoss()
-    for epoch in range(epochs):
 
-        model.train()
-        optimizer.zero_grad()
+    train_loss_vec = np.zeros(epochs)
+    test_loss_vec = np.zeros(epochs)
+    model.train()
+    for epoch in tqdm(range(epochs), desc='Epochs'):
+        adjust_learning_rate(optimizer, epoch)
+        train_loss = 0.0
+        for lstm_batch, dense_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            pred = model(lstm_batch, dense_batch)
+            loss = loss_fn(pred, target_batch.unsqueeze(1))  # modified shape because threw errors
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            train_loss += loss.item() * lstm_batch.size(0)
 
-        pred = model(lstm_part_tensor, dense_part)
-        loss = loss_fn(pred, true)
+        train_loss /= len(train_loader)
+        train_loss_vec[epoch] = loss.item() * lstm_batch.size(0)
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        loss_vec[epoch] += loss.item()
+        model.eval()
+        val_loss = 0.0
+        with torch.no_grad():
+            for lstm_batch, dense_batch, target_batch in test_loader:
+                pred = model(lstm_batch, dense_batch)
+                loss = loss_fn(pred, target_batch.unsqueeze(1))
+                val_loss += loss.item() * lstm_batch.size(0)
+
+        val_loss /= len(test_loader)
+        test_loss_vec[epoch] = loss.item() * lstm_batch.size(0)
+
+        print(f'Epoch {epoch + 1}/{epochs}, Training Loss: {train_loss:.4f}, Validation Loss: {val_loss:.4f}')
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(range(1, epochs + 1), np.log(train_loss_vec), label='Training Loss')
+    plt.plot(range(1, epochs + 1), np.log(test_loss_vec), label='Validation Loss')
+    plt.title('LSTM Call Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('log(MSE)')
+    plt.legend()
+    plt.show()
+    return model
+
+def adjust_learning_rate(optimizer, epoch):
+    """adjusts the learning rate according to the specified schedule."""
+    if epoch < 10:
+        lr = 1e-2
+    elif epoch < 15:
+        lr = 1e-3
+    else:
+        lr = 1e-4
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+    print(f"Epoch {epoch+1}: Learning rate adjusted to {lr}")
+
 
 
 if __name__ == "__main__":
@@ -126,8 +201,23 @@ if __name__ == "__main__":
 
     path_to_underlying_data = Path(__file__).parent.parent / "clean_data" / "underlying.csv"
 
-    final_df, calls_df, puts_df = preprocess_data(path_to_options_data, path_to_underlying_data)
-    X_train, cX_test, cy_train, cy_test = get_train_test_split(calls_df)
+    calls_df = preprocess_data(path_to_options_data, path_to_underlying_data, option_type='c')
+    cX_train, cX_test, cy_train, cy_test = get_train_test_split(calls_df, option_type='c')
 
     model = LSTMTrain()
-    training_loop(20, model, X_train=X_train, y_train=cy_train)
+    model_trained = training_loop(30, model, X_train=cX_train, y_train=cy_train, X_test=cX_test, y_test=cy_test)
+
+
+    torch.save(model_trained.state_dict(), './model_trained.pth')
+
+    lstm_test_tensor = torch.tensor(cX_test.drop(columns=['strike', 'Time_to_Maturity', 'RF_Rate']).values,
+                                    dtype=torch.float32)
+    dense_test_tensor = torch.tensor(cX_test[['strike', 'Time_to_Maturity', 'RF_Rate']].values, dtype=torch.float32)
+    test_targets = torch.tensor(cy_test.values, dtype=torch.float32)
+    cy_pred = model_trained(lstm_test_tensor, dense_test_tensor)
+
+    cy_pred = cy_pred.squeeze(1).detach().numpy()
+
+    diff_sq = np.square(cy_test.to_numpy() - cy_pred)
+    mse = np.mean(diff_sq)
+    print(calculate_metrics(cy_test.to_numpy(), cy_pred))
